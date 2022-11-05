abc071926e3a7bd5d3e33e9ebd0bb798d0fcf747
Karthikeyan <karthikeyan.as@lntinfotech.com>
2022-11-06 01:08:27 +0530

    |000|Karthikeyan|DB_name_issue_fixed|

--- src/main/java/com/cts/eauction/EauctionApplication.java
+++ src/main/java/com/cts/eauction/EauctionApplication.java
@@ -1,13 +1,17 @@
 package com.cts.eauction;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.cts.eauction.repository.ProductRepository;
 
+
 @SpringBootApplication
-@EnableMongoRepositories(basePackageClasses = ProductRepository.class)
 public class EauctionApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/cts/eauction/controller/ProductController.java
+++ src/main/java/com/cts/eauction/controller/ProductController.java
@@ -8,21 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cts.eauction.impl.ProductServiceImpl;
 import com.cts.eauction.model.Product;
-import com.cts.eaution.impl.ProductServiceImpl;
+import com.cts.eauction.repository.ProductRepository;
+import com.cts.eauction.service.ProductService;
 
-@CrossOrigin(origins = "*", maxAge = 4800)
 @RestController
 @RequestMapping("/e-auction/api/v1/seller/")
 public class ProductController {
 	
 	@Autowired
-	public ProductServiceImpl productServiceImpl;
-	
+	public ProductService productService;
+
 	@GetMapping("/show-bids")
 	public List<Product> getAllProducts() {
 		System.out.println("Hello Product...");
-		return productServiceImpl.findAll();
+		return productService.findAll();
 	}
 	
 	
